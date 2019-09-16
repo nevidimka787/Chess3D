998a6adc27ffcf1d69e050d0e77b7dca0f2d14bb
nevidimka787 <55407807+nevidimka787@users.noreply.github.com>
2019-09-16 21:38:28 +0300

    Update List.h
    Create and store game space's informations, configs and game's rules.

--- Libraries/List.h
+++ Libraries/List.h
@@ -5,10 +5,10 @@ public:
 	List(int type_of_game);
 	int Get(int number);
 private:
-	int *number = new int;
 	int *lenght = new int;
 	int *weight = new int;
 	int *height = new int;
+	int *number = new int;
 	int *type = new int;; //lenght = X; weight = Y; height = Z;
 	int *entities = new int[8 * 8 * 8];//(X, Y, Z) == points[X + Y * 8 + Z * 64]  start = (0, 0, 0)
 };
