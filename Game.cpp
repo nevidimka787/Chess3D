5faee29523365c30546806c23f1c343064665207
nevidimka787 <55407807+nevidimka787@users.noreply.github.com>
2019-09-19 23:19:32 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,18 +1,22 @@
-#include"Liraries/GameSpace.h"
-#include"Liraries/List.h"
+
+#include"Libraries/GameSpace.h"
+#include"Libraries/List.h"
 
 #include<iostream>
 #include<string>
-
 using namespace std;
 
 int main()
 {
-	GameSpace *gamespase1 = new GameSpace(1);
+	bool* game_go = new bool;
+	*game_go = true;
+	GameSpace* gamespase1 = new GameSpace(1);
+	int* figure_x = new int, * figure_y = new int, * figure_z = new int, * target_x = new int, * target_y = new int, * target_z = new int;
+	cout << "Game start." << endl;
 	gamespase1->ShowInformation();
 	gamespase1->ShowAll();
 	gamespase1->MoveWhite();
-	int *figure_x = new int, *figure_y = new int, *figure_z = new int, *target_x = new int, *target_y = new int, *target_z = new int;
+	cout << "White's move." << endl;
 	while (true)
 	{
 		cin >> *figure_x >> *figure_y >> *figure_z >> *target_x >> *target_y >> *target_z;
@@ -25,7 +29,31 @@ int main()
 		gamespase1->SetPoint(*figure_x, *figure_y, *figure_z, 0);
 		gamespase1->ShowInformation();
 		gamespase1->ShowAll();
+		if (gamespase1->Mat(true))
+		{
+			cout << "White win!" << endl;
+			break;
+		}
+		gamespase1->MoveBlack();
+		cout << "Black's move.";
+		cin >> *figure_x >> *figure_y >> *figure_z >> *target_x >> *target_y >> *target_z;
+		while (!gamespase1->MeetGameRule(*figure_x, *figure_y, *figure_z, *target_x, *target_y, *target_z))
+		{
+			cout << "Move is possible." << endl;
+			cin >> *figure_x >> *figure_y >> *figure_z >> *target_x >> *target_y >> *target_z;
+		}
+		gamespase1->ReplasePoints(*figure_x, *figure_y, *figure_z, *target_x, *target_y, *target_z);
+		gamespase1->SetPoint(*figure_x, *figure_y, *figure_z, 0);
+		gamespase1->ShowInformation();
+		gamespase1->ShowAll();
+		if (gamespase1->Mat(false))
+		{
+			cout << "Black win!" << endl;
+			break;
+		}
 		gamespase1->MoveWhite();
-	}
+		cout << "White's move." << endl;
+		}
+	cout << "Game over" << endl;
+	cin >> *game_go;
 }
-
