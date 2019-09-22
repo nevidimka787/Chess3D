27b3c802705d25a61ea82c4d2151cc5d91b99fda
nevidimka787 <55407807+nevidimka787@users.noreply.github.com>
2019-09-22 22:28:45 +0300

    Update GameSpace.h

--- Libraries/GameSpace.h
+++ Libraries/GameSpace.h
@@ -16,13 +16,14 @@ public:
 	void MoveWhite();//White party is moving now.
 	void MoveBlack();//Black party is moving now.
 	bool MeetGameRule(int position, int end_position);//Figure's movement between positions on line meet game rules.
+	bool MeetGameRule(int x_start, int y_start, int z_start, int number);//Figure's movement between position and position on line meet game rules.
 	bool MeetGameRule(int x_start, int y_start, int z_start, int x_end, int y_end, int z_end);//Figure's movement between positions meet game rules.
 	void ReplasePoints(int number1, int number2);//Take a figure from first position on line and replaset this with figure from second position on line.
 	void ReplasePoints(int x1, int y1, int z1, int number2);//Take a figure from first position and replaset this with figure from second position on line.
 	void ReplasePoints(int x1, int y1, int z1, int x2, int y2, int z2);//Take a figure from first position and replaset this with figure from second position.
 	void SetPoint(int number, int number_of_figure);//Make a figure in the indicated position on line.
 	void SetPoint(int x, int y, int z, int number_of_figure);//Make a figure in the indicated position.
-	void Show(int plane_z);//Show information about indikated plate.
+	void Show(int plane_z, bool show_cordinats_vectors);//Show information about indikated plate.
 	void ShowAll();//Show information about all plates.
 	void ShowInformation();//Show dependence between figure's name and figure's number of type.
 	bool Way();//Verify way in the game.
@@ -32,8 +33,8 @@ private:
 	int* weight = new int;
 	int* height = new int;
 	int* type = new int; //lenght = X; weight = Y; height = Z;
-	int* points = new int[8 * 8 * 8];//(X, Y, Z) == points[X + Y * 8 + Z * 64]  start = (0, 0, 0)
-	bool* start_points = new bool[8 * 8 * 8];
+	int* points = new int[512 * 512 * 512];//(X, Y, Z) == points[X + Y * 8 + Z * 64]  start = (0, 0, 0)
+	bool* start_points = new bool[512 * 512 * 512];
 	bool* white_move = new bool;
 	bool* black_move = new bool;
 	bool* game = new bool;
