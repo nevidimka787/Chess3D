39d7d524d07d1e4e86ecf06cf44a5799db61e5c8
nevidimka787 <55407807+nevidimka787@users.noreply.github.com>
2019-09-22 22:30:10 +0300

    Update List.h

--- Libraries/List.h
+++ Libraries/List.h
@@ -10,5 +10,5 @@ private:
 	int* weight = new int;
 	int* height = new int;
 	int* type = new int;
-	int* entities = new int[8 * 8 * 8];//(X, Y, Z) == points[X + Y * 8 + Z * 64]  start = (0, 0, 0)
+	int* entities = new int[512, 512, 512];//(X, Y, Z) == points[X + Y * 8 + Z * 64]  start = (0, 0, 0)
 };
