efb384aeaa420879d3cc6652ad8d6967921d6f58
nevidimka787 <55407807+nevidimka787@users.noreply.github.com>
2019-09-22 12:01:45 +0300

    Update List.h

--- Libraries/List.h
+++ Libraries/List.h
@@ -4,11 +4,11 @@ class List
 public:
 	List(int type_of_game);
 	int Get(int number);
+	int GetElement(int number);//0 - lenght; 1 - weight; 2 - height; 3 - type;
 private:
-	int *lenght = new int;
-	int *weight = new int;
-	int *height = new int;
-	int *number = new int;
-	int *type = new int;; //lenght = X; weight = Y; height = Z;
-	int *entities = new int[8 * 8 * 8];//(X, Y, Z) == points[X + Y * 8 + Z * 64]  start = (0, 0, 0)
+	int* lenght = new int;
+	int* weight = new int;
+	int* height = new int;
+	int* type = new int;
+	int* entities = new int[8 * 8 * 8];//(X, Y, Z) == points[X + Y * 8 + Z * 64]  start = (0, 0, 0)
 };
