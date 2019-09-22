1f3b66c02d9e76ee0df9864777dc1805880360bc
nevidimka787 <55407807+nevidimka787@users.noreply.github.com>
2019-09-22 12:02:06 +0300

    Update List.cpp

--- Libraries/List.cpp
+++ Libraries/List.cpp
@@ -38,12 +38,11 @@ List::List(int type_of_game)
 		-9 - Rook
 		-10 - Pawn
 		*/
-		for (int n = 0; n < 512; n++)
+		for (int n = 0; n < *lenght * *weight * *height; n++)
 		{
 			entities[n] = 0;
 		}
 		/*
-
 			0	1	2	3	4	5	6	7 >	Y
 		0
 
@@ -62,7 +61,6 @@ List::List(int type_of_game)
 		7
 		v
 		Z
-
 		*/
 		for (int x = 0; x < 2; x++)
 		{
@@ -70,7 +68,7 @@ List::List(int type_of_game)
 			{
 				for (int z = 1; z < 7; z++)
 				{
-					entities[x + y * 8 + z * 64] = 10;//(0, 1 , 1):(1, 6, 6) -> WHITE pawns
+					entities[x + y * *lenght + z * *lenght * *weight] = 10;//(0, 1 , 1):(1, 6, 6) -> WHITE pawns
 				}
 			}
 		}
@@ -80,50 +78,50 @@ List::List(int type_of_game)
 			{
 				for (int z = 1; z < 7; z++)
 				{
-					entities[x + y * 8 + z * 64] = -10;//(6, 1 , 1):(7, 6, 6) -> BLACK pawns
+					entities[x + y * *lenght + z * *lenght * *weight] = -10;//(6, 1 , 1):(7, 6, 6) -> BLACK pawns
 				}
 			}
 		}
-		entities[0 + 2 * 8 + 2 * 64] = 9;//WHITE rooks
-		entities[0 + 5 * 8 + 2 * 64] = 9;
-		entities[0 + 2 * 8 + 5 * 64] = 9;
-		entities[0 + 5 * 8 + 5 * 64] = 9;
-		entities[7 + 2 * 8 + 2 * 64] = -9;//BLACK rooks
-		entities[7 + 5 * 8 + 2 * 64] = -9;
-		entities[7 + 2 * 8 + 5 * 64] = -9;
-		entities[7 + 5 * 8 + 5 * 64] = -9;
+		entities[0 + 2 * *lenght + 2 * *lenght * *weight] = 9;//WHITE rooks
+		entities[0 + 5 * *lenght + 2 * *lenght * *weight] = 9;
+		entities[0 + 2 * *lenght + 5 * *lenght * *weight] = 9;
+		entities[0 + 5 * *lenght + 5 * *lenght * *weight] = 9;
+		entities[7 + 2 * *lenght + 2 * *lenght * *weight] = -9;//BLACK rooks
+		entities[7 + 5 * *lenght + 2 * *lenght * *weight] = -9;
+		entities[7 + 2 * *lenght + 5 * *lenght * *weight] = -9;
+		entities[7 + 5 * *lenght + 5 * *lenght * *weight] = -9;
 
-		entities[0 + 4 * 8 + 2 * 64] = 8;//WHITE knigth
-		entities[0 + 3 * 8 + 5 * 64] = 8;
-		entities[7 + 4 * 8 + 2 * 64] = -8;//BLACK knigth
-		entities[7 + 3 * 8 + 5 * 64] = -8;
+		entities[0 + 4 * *lenght + 2 * *lenght * *weight] = 8;//WHITE knigth
+		entities[0 + 3 * *lenght + 5 * *lenght * *weight] = 8;
+		entities[7 + 4 * *lenght + 2 * *lenght * *weight] = -8;//BLACK knigth
+		entities[7 + 3 * *lenght + 5 * *lenght * *weight] = -8;
 
-		entities[0 + 2 * 8 + 3 * 64] = 7;//WHITE knigth diagonal
-		entities[0 + 5 * 8 + 4 * 64] = 7;
-		entities[7 + 2 * 8 + 3 * 64] = -7;//BLACK knigth diagonal
-		entities[7 + 5 * 8 + 4 * 64] = -7;
+		entities[0 + 2 * *lenght + 3 * *lenght * *weight] = 7;//WHITE knigth diagonal
+		entities[0 + 5 * *lenght + 4 * *lenght * *weight] = 7;
+		entities[7 + 2 * *lenght + 3 * *lenght * *weight] = -7;//BLACK knigth diagonal
+		entities[7 + 5 * *lenght + 4 * *lenght * *weight] = -7;
 
-		entities[0 + 2 * 8 + 4 * 64] = 6;//WHITE bishop
-		entities[0 + 5 * 8 + 3 * 64] = 6;
-		entities[7 + 2 * 8 + 4 * 64] = -6;//BLACK bishop
-		entities[7 + 5 * 8 + 3 * 64] = -6;
+		entities[0 + 2 * *lenght + 4 * *lenght * *weight] = 6;//WHITE bishop
+		entities[0 + 5 * *lenght + 3 * *lenght * *weight] = 6;
+		entities[7 + 2 * *lenght + 4 * *lenght * *weight] = -6;//BLACK bishop
+		entities[7 + 5 * *lenght + 3 * *lenght * *weight] = -6;
 
-		entities[0 + 3 * 8 + 2 * 64] = 5;//WHITE bishop diagonal
-		entities[0 + 4 * 8 + 5 * 64] = 5;
-		entities[7 + 3 * 8 + 2 * 64] = -5;//BLACK bishop diagonal
-		entities[7 + 4 * 8 + 5 * 64] = -5;
+		entities[0 + 3 * *lenght + 2 * *lenght * *weight] = 5;//WHITE bishop diagonal
+		entities[0 + 4 * *lenght + 5 * *lenght * *weight] = 5;
+		entities[7 + 3 * *lenght + 2 * *lenght * *weight] = -5;//BLACK bishop diagonal
+		entities[7 + 4 * *lenght + 5 * *lenght * *weight] = -5;
 
-		entities[0 + 3 * 8 + 4 * 64] = 4;//WHITE queen
-		entities[7 + 3 * 8 + 4 * 64] = -4;//BLACK queen
+		entities[0 + 3 * *lenght + 4 * *lenght * *weight] = 4;//WHITE queen
+		entities[7 + 3 * *lenght + 4 * *lenght * *weight] = -4;//BLACK queen
 
-		entities[0 + 4 * 8 + 3 * 64] = 3;//WHITE queen diagonal
-		entities[7 + 4 * 8 + 3 * 64] = -3;//BLACK queen diagonal
+		entities[0 + 4 * *lenght + 3 * *lenght * *weight] = 3;//WHITE queen diagonal
+		entities[7 + 4 * *lenght + 3 * *lenght * *weight] = -3;//BLACK queen diagonal
 
-		entities[0 + 3 * 8 + 3 * 64] = 2;//WHITE super queen
-		entities[7 + 3 * 8 + 3 * 64] = -2;//BLACK super queen
+		entities[0 + 3 * *lenght + 3 * *lenght * *weight] = 2;//WHITE super queen
+		entities[7 + 3 * *lenght + 3 * *lenght * *weight] = -2;//BLACK super queen
 
-		entities[0 + 4 * 8 + 4 * 64] = 1;//WHITE king
-		entities[7 + 4 * 8 + 4 * 64] = -1;//BLACK king
+		entities[0 + 4 * *lenght + 4 * *lenght * *weight] = 1;//WHITE king
+		entities[7 + 4 * *lenght + 4 * *lenght * *weight] = -1;//BLACK king
 	}
 	default:
 		break;
@@ -139,5 +137,33 @@ int List::Get(int number)
 	else
 	{
 		return 0;
+	}
+}
+
+int List::GetElement(int number)
+{
+	switch (number)
+	{
+	case 0:
+	{
+		return *lenght;
+		break;
+	}
+	case 1:
+	{
+		return *weight;
+		break;
+	}
+	case 2:
+	{
+		return *height;
+		break;
+	}
+	case 3:
+	{
+		return *type;
+		break;
+	}
+	return 0;
 	}
 }
